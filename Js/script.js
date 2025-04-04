6b9bf865206d9e85112323fb6a82fec5c5000734
coder-nishu <nishatkh1582@gmail.com>
2025-04-05 01:13:47 +0600

    modal added

--- Js/script.js
+++ Js/script.js
@@ -6,7 +6,8 @@ seatButtons.forEach((i) => {
     i.addEventListener('click', () => {
         seatSelectionHandler(i.id);
         updateBtnStateOnMaxSelection();
-        updateSeatInfoState()
+        updateSeatInfoState();
+        nextBtn();
     })
 
 })
@@ -47,20 +48,59 @@ document.getElementById('coupon-btn').addEventListener('click', () => {
     const coupon2 = 'couple20'
     const inputBox = document.getElementById('coupon-input')
     const inputVal = inputBox.value
-    const price_4person = 550*4
+    const price_4person = 550 * 4
     const g_price = document.getElementById('grand-price')
-    if(inputVal.toLowerCase() === coupon1 ){
-        g_price.innerText = (price_4person - (price_4person*0.15))
+    if (inputVal.toLowerCase() === coupon1) {
+        g_price.innerText = (price_4person - (price_4person * 0.15))
         const couponBox = document.getElementById('coupon-box')
         couponBox.classList.add('hidden')
-    }else if (inputVal.toLowerCase() === coupon2){
-        g_price.innerText = (price_4person - (price_4person*0.2))
+    } else if (inputVal.toLowerCase() === coupon2) {
+        g_price.innerText = (price_4person - (price_4person * 0.2))
         const couponBox = document.getElementById('coupon-box')
         couponBox.classList.add('hidden')
     }
-    else{
+    else {
         alert("Coupon Code Is Not Valid.Try Again")
     }
-    inputBox.value = ''
-    
+    inputBox.value = '';
+
 })
+// buy ticket button event 
+document.getElementById('btn-buy-ticket').addEventListener('click', () => {
+    document.getElementById('ticket-buying-section').scrollIntoView({ behavior: "smooth" })
+})
+
+function nextBtn(){
+    const numField = document.getElementById('input-phn')
+    const numVal = numField.value
+    const btnNext = document.getElementById('next-btn')
+    if((numVal !== "") && selectedSeats.length > 0){
+        btnNext.disabled = false
+    }else{
+        btnNext.disabled = true
+    }
+}
+
+document.addEventListener('keyup', function () {
+    const numField = document.getElementById('input-phn')
+    const numVal = numField.value
+    const btnNext = document.getElementById('next-btn')
+    if ((numVal !== "") && selectedSeats.length > 0) {
+        btnNext.disabled = false
+    } else {
+        btnNext.disabled = true
+    }
+});
+document.getElementById('next-btn').addEventListener('click', function () {
+    const numField = document.getElementById('input-phn')
+    const numVal = numField.value
+    const phnIncorrect = document.getElementById('phn-incorrect')
+    if(numVal.length === 11){
+        document.getElementById('my_modal_1').showModal()
+        phnIncorrect.classList.add('hidden')
+    }else{
+        phnIncorrect.classList.remove('hidden')
+    }
+    
+});
+
